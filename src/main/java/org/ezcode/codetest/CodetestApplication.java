6d28ed0dfce95f2a210604bf907efd760fb8dde1
pokerbearkr <ogdongwon@gmail.com>
2025-05-28 16:42:44 +0900

    docs : 초기 문서 작업 readme PR template

--- src/main/java/org/ezcode/codetest/CodetestApplication.java
+++ src/main/java/org/ezcode/codetest/CodetestApplication.java
@@ -2,8 +2,12 @@ package org.ezcode.codetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
 public class CodetestApplication {
 
 	public static void main(String[] args) {
