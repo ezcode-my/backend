b08b8a7ea0708f6d5b0f10bcc85eee5209651d36
chat26666 <106295579+chat26666@users.noreply.github.com>
2025-05-29 19:48:01 +0900

    MBA-7 : 테스트 #comment 지라 연동 테스트1234 (#3)

--- src/main/java/org/ezcode/codetest/infrastructure/persitence/repository/chat/ChatRoomRepositoryImpl.java
+++ src/main/java/org/ezcode/codetest/infrastructure/persitence/repository/chat/ChatRoomRepositoryImpl.java
@@ -1,6 +1,27 @@
 package org.ezcode.codetest.infrastructure.persitence.repository.chat;
 
 import org.ezcode.codetest.application.chatting.port.repository.ChatRoomRepository;
+import org.ezcode.codetest.domain.chat.model.ChatRoom;
+import org.springframework.stereotype.Repository;
 
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
 public class ChatRoomRepositoryImpl implements ChatRoomRepository {
+
+	private final ChatRoomJpaRepository chatRoomRepository;
+
+	public ChatRoom save(ChatRoom chatRoom) {
+
+		return chatRoomRepository.save(chatRoom);
+	}
+
+	public ChatRoom findOrElseThrow(Long id) {
+
+		return chatRoomRepository.findById(id).orElseThrow(() ->
+			new RuntimeException("해당 Entity를 찾을 수 없습니다. id = " + id));
+		// TODO: 프로젝트에서 사용하는 타입의 exception과 status를 던져줘야 함
+	}
+
 }
