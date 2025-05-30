43c3f9d90c4bf730e3c8f7ac1538dab79da6679d
thezz9 <thezz9@naver.com>
2025-05-30 19:43:46 +0900

    feat: Swagger JWT 기반 인증 기능 추가

---
+++ src/main/java/org/ezcode/codetest/infrastructure/swagger/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package org.ezcode.codetest.infrastructure.swagger.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(
+	info = @Info(title = "API 문서", version = "v1"),
+	security = @SecurityRequirement(name = "JWT")
+)
+@SecurityScheme(
+	name = "JWT",
+	type = SecuritySchemeType.HTTP,
+	scheme = "bearer",
+	bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
