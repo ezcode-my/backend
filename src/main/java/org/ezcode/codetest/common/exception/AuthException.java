aada711061f85636a5a7372d0271aabcfffc8cc4
minjee2758 <minjee2758@naver.com>
2025-05-30 11:52:40 +0900

    feature : jwt 기본 세팅

---
+++ src/main/java/org/ezcode/codetest/common/exception/AuthException.java
@@ -0,0 +1,7 @@
+package org.ezcode.codetest.common.exception;
+
+public class AuthException extends RuntimeException {
+	public AuthException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/org/ezcode/codetest/domain/user/service/AuthDomainService.java
@@ -0,0 +1,7 @@
+package org.ezcode.codetest.domain.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthDomainService {
+}
