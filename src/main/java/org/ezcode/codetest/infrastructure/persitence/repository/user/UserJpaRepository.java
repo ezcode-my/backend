fe4e4fa96feaa7c43157e2a7708245744991404f
minjee2758 <minjee2758@naver.com>
2025-05-31 18:10:25 +0900

    refactor : 불필요한 import 삭제

--- src/main/java/org/ezcode/codetest/infrastructure/persitence/repository/user/UserJpaRepository.java
+++ src/main/java/org/ezcode/codetest/infrastructure/persitence/repository/user/UserJpaRepository.java
@@ -2,7 +2,6 @@ package org.ezcode.codetest.infrastructure.persitence.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ezcode.codetest.domain.user.model.entity.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
