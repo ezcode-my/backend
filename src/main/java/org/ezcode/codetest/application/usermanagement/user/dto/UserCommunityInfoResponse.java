6cb18ae3fd2d15465293d71b31049b611dd883b8
minjee2758 <minjee2758@naver.com>
2025-06-02 11:21:48 +0900

    feature : community 기능에서 제공할 유저 정보를 반환하는 responseDto 추가 - UserCommunityResponse

---
+++ src/main/java/org/ezcode/codetest/application/usermanagement/user/dto/UserCommunityInfoResponse.java
@@ -0,0 +1,22 @@
+package org.ezcode.codetest.application.usermanagement.user.dto;
+
+import org.ezcode.codetest.domain.user.model.enums.Tier;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class UserCommunityInfoResponse {
+	private final Long userId;
+	private final String nickname;
+	private final Tier tier;
+	private final String profileImageUrl;
+
+	@Builder
+	public UserCommunityInfoResponse(Long userId, String nickname, Tier tier, String profileImageUrl) {
+		this.userId = userId;
+		this.nickname = nickname;
+		this.tier = tier;
+		this.profileImageUrl = profileImageUrl;
+	}
+}
