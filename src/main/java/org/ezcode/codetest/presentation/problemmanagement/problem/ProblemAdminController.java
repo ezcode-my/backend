e681a8fd91df08dc8a038e935ee4e77712557f20
kimminu7 <rock9024@naver.com>
2025-06-02 19:12:34 +0900

    refactor : 패키지 분리 ( 테스트 케이스, 문제 )

--- src/main/java/org/ezcode/codetest/presentation/problemmanagement/problem/ProblemAdminController.java
+++ src/main/java/org/ezcode/codetest/presentation/problemmanagement/problem/ProblemAdminController.java
@@ -1,4 +1,4 @@
-package org.ezcode.codetest.presentation.problemmanagement;
+package org.ezcode.codetest.presentation.problemmanagement.problem;
 
 import org.ezcode.codetest.application.problem.dto.request.ProblemCreateRequest;
 import org.ezcode.codetest.application.problem.dto.request.ProblemUpdateRequest;
