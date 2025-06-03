560762a17cb150ab0b9af1008357017c65caea1b
SeungWoo Ryu <swstar21c@gmail.com>
2025-06-04 03:45:25 +0900

    feat : 코드 중복 제거를 위한 공통 추상 클래스 구현
    - discussion과 reply의 추천 기능은 사용하는 테이블만 다를 뿐 기능과 코드는 거의 동일하다.
      - 각자 별개로 구현할 경우 코드 중복 발생
      - 추후 solution 탭을 개설할 경우 중복 코드는 더 많이 늘어난다.
    - 이를 해결하기 위해 테이블은 분리하되, 공통 로직은 추상 클래스로 묶어서 사용할 수 있도록 했다.
    - 핵심 비즈니스 로직은 공통 코드를 사용하므로 중복이 많이 제거됐다.

---
+++ src/main/java/org/ezcode/codetest/domain/community/repository/BaseVoteRepository.java
@@ -0,0 +1,19 @@
+package org.ezcode.codetest.domain.community.repository;
+
+import java.util.Optional;
+
+import org.ezcode.codetest.domain.community.model.BaseVote;
+
+public interface BaseVoteRepository<T extends BaseVote> {
+
+	T save(T voteEntity);
+
+	// existBy~가 더 효율적이긴 하지만
+	// 나중에 비추천 기능까지 생기면 재사용 가능
+	Optional<T> findByVoterIdAndTargetId(Long voterId, Long targetId);
+
+	boolean existByVoterIdAndTargetId(Long voterId, Long targetId);
+
+	void delete(T voteEntity);
+
+}
---
+++ src/main/java/org/ezcode/codetest/domain/community/service/BaseVoteDomainService.java
@@ -0,0 +1,34 @@
+package org.ezcode.codetest.domain.community.service;
+
+import java.util.Optional;
+
+import org.ezcode.codetest.domain.community.model.BaseVote;
+import org.ezcode.codetest.domain.community.repository.BaseVoteRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public abstract class BaseVoteDomainService<T extends BaseVote, R extends BaseVoteRepository<T>> {
+
+	protected final R repository;
+
+	public T createVoteEntity(T entity) {
+
+		return repository.save(entity);
+	}
+
+	public Optional<T> getVoteEntity(Long voterId, Long targetId) {
+
+		return repository.findByVoterIdAndTargetId(voterId, targetId);
+	}
+
+	public boolean getVoteStatus(Long voterId, Long targetId) {
+
+		return repository.existByVoterIdAndTargetId(voterId, targetId);
+	}
+
+	public void removeVoteEntity(T entity) {
+
+		repository.delete(entity);
+	}
+}
