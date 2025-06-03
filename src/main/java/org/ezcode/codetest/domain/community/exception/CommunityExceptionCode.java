eaaee658ba0109a4da915641c9eef61a1e0510d5
SeungWoo Ryu <swstar21c@gmail.com>
2025-06-03 17:42:11 +0900

    feat : 댓글 관련 예외 코드 추가

--- src/main/java/org/ezcode/codetest/domain/community/exception/CommunityExceptionCode.java
+++ src/main/java/org/ezcode/codetest/domain/community/exception/CommunityExceptionCode.java
@@ -10,9 +10,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CommunityExceptionCode implements ResponseCode {
 
-	DISCUSSION_NOT_FOUND(false, HttpStatus.NOT_FOUND, "해당 ID의 글이 존재하지 않습니다."),
+	DISCUSSION_NOT_FOUND(false, HttpStatus.NOT_FOUND, "해당 ID의 자유글이 존재하지 않습니다."),
 	DISCUSSION_PROBLEM_MISMATCH(false, HttpStatus.BAD_REQUEST, "해당 글이 요청된 문제에 속하지 않습니다."),
-	USER_NOT_AUTHOR(false, HttpStatus.FORBIDDEN, "작성자만 수정/삭제할 수 있습니다.");
+	
+	REPLY_NOT_FOUND(false, HttpStatus.NOT_FOUND, "해당 ID의 댓글이 존재하지 않습니다."),
+	REPLY_DISCUSSION_MISMATCH(false, HttpStatus.BAD_REQUEST, "해당 댓글이 요청된 자유글에 속하지 않습니다."),
+	
+	USER_NOT_AUTHOR(false, HttpStatus.FORBIDDEN, "작성자만 수정/삭제할 수 있습니다."),
+	;
 
 	private final boolean success;
 	private final HttpStatus status;
