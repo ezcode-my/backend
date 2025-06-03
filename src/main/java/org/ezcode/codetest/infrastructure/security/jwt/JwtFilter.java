be4068544157e88635d89bfd0a5b93785ccdcc16
minjee2758 <minjee2758@naver.com>
2025-06-03 14:44:29 +0900

    feature : Filter에서  에러처리만 담당하는 클래스 추가

--- src/main/java/org/ezcode/codetest/infrastructure/security/jwt/JwtFilter.java
+++ src/main/java/org/ezcode/codetest/infrastructure/security/jwt/JwtFilter.java
@@ -39,7 +39,6 @@ public class JwtFilter extends OncePerRequestFilter {
 									FilterChain filterChain) throws ServletException, IOException {
 
 		String bearerToken = request.getHeader("Authorization");
-		String url = request.getRequestURI();
 
 		if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
 			filterChain.doFilter(request, response);
@@ -47,54 +46,38 @@ public class JwtFilter extends OncePerRequestFilter {
 		}
 
 		String jwt = jwtUtilImpl.substringToken(bearerToken);
+		Claims claims = jwtUtilImpl.extractClaims(jwt);
 
-		try {
-			Claims claims = jwtUtilImpl.extractClaims(jwt);
-
-			if(claims == null){
-				response.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 JWT 토큰입니다");
-				return;
-			}
-
-			UserRole userRole = UserRole.valueOf(claims.get("userRole", String.class));
-			String tierClaim = claims.get("tier", String.class);
-			Tier tier = tierClaim != null ? Tier.valueOf(tierClaim) : Tier.NEWBIE;
-
-			AuthUser authUser = new AuthUser(
-				Long.parseLong(claims.getSubject()),
-				(String) claims.get("username"),
-				(String) claims.get("nickname"),
-				(String) claims.get("email"),
-				userRole,
-				tier
-			);
-
-			// 인가를 위한 권한 정보 저장
-			Collection<? extends GrantedAuthority> authorities =
-				List.of(new SimpleGrantedAuthority("ROLE_" + authUser.getRole().name()));
-
-			// Spring Security 인증 토큰 생성
-			Authentication authToken = new UsernamePasswordAuthenticationToken(authUser,
-				null, authorities);
-
-			// SecurityContextHolder(세션)에 토큰 담기
-			SecurityContextHolder.getContext().setAuthentication(authToken);
-
-
-			filterChain.doFilter(request, response);
-		} catch (SecurityException | MalformedJwtException e) {
-			log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.", e);
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
-		} catch (ExpiredJwtException e) {
-			log.error("Expired JWT token, 만료된 JWT token 입니다.", e);
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 JWT 토큰입니다.");
-		} catch (UnsupportedJwtException e) {
-			log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.", e);
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "지원되지 않는 JWT 토큰입니다.");
-		} catch (Exception e) {
-			log.error("Internal server error", e);
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		if (claims == null) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 JWT 토큰입니다");
+			return;
 		}
-	}
 
-}
+		UserRole userRole = UserRole.valueOf(claims.get("userRole", String.class));
+		String tierClaim = claims.get("tier", String.class);
+		Tier tier = tierClaim != null ? Tier.valueOf(tierClaim) : Tier.NEWBIE;
+
+		AuthUser authUser = new AuthUser(
+			Long.parseLong(claims.getSubject()),
+			(String)claims.get("username"),
+			(String)claims.get("nickname"),
+			(String)claims.get("email"),
+			userRole,
+			tier
+		);
+
+		// 인가를 위한 권한 정보 저장
+		Collection<? extends GrantedAuthority> authorities =
+			List.of(new SimpleGrantedAuthority("ROLE_" + authUser.getRole().name()));
+
+		// Spring Security 인증 토큰 생성
+		Authentication authToken;
+		authToken = new UsernamePasswordAuthenticationToken(authUser, null, authorities);
+
+		// SecurityContextHolder(세션)에 토큰 담기
+		SecurityContextHolder.getContext().setAuthentication(authToken);
+
+		filterChain.doFilter(request, response);
+		}
+
+	}
