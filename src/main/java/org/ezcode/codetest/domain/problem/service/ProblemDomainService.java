69d3f283c7cb05e82fc028e3315bb48646f1fe7c
kimminu7 <rock9024@naver.com>
2025-06-03 15:52:40 +0900

    refactor : 예외처리 클래스 적용 ( 문제 ) + 주석 설명

--- src/main/java/org/ezcode/codetest/domain/problem/service/ProblemDomainService.java
+++ src/main/java/org/ezcode/codetest/domain/problem/service/ProblemDomainService.java
@@ -1,5 +1,7 @@
 package org.ezcode.codetest.domain.problem.service;
 
+import org.ezcode.codetest.domain.problem.exception.ProblemException;
+import org.ezcode.codetest.domain.problem.exception.code.ProblemExceptionCode;
 import org.ezcode.codetest.domain.problem.model.entity.Problem;
 import org.ezcode.codetest.domain.problem.model.enums.Category;
 import org.ezcode.codetest.domain.problem.repository.ProblemRepository;
@@ -7,7 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -33,7 +34,7 @@ public class ProblemDomainService {
 	public Problem getProblem(Long problemId) {
 
 		return problemRepository.findByIdNotDeleted(problemId)
-			.orElseThrow(() -> new EntityNotFoundException("문제를 찾을수 없습니다."));
+			.orElseThrow(() -> new ProblemException(ProblemExceptionCode.PROBLEM_NOT_FOUND));
 	}
 
 	public void removeProblem(Problem problem) {
