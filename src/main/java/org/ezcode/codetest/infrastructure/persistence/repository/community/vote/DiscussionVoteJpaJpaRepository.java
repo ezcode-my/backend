163e8f7f3014bd14e7196bd252ed5561c2fcd3a3
SeungWoo Ryu <swstar21c@gmail.com>
2025-06-04 03:35:50 +0900

    refactor : 기존 소스 코드 패키지 이동
    - infra 측 소스 코드가 많아져서 패키지로 정리함

---
+++ src/main/java/org/ezcode/codetest/infrastructure/persistence/repository/community/vote/DiscussionVoteJpaJpaRepository.java
@@ -0,0 +1,14 @@
+package org.ezcode.codetest.infrastructure.persistence.repository.community.vote;
+
+import java.util.Optional;
+
+import org.ezcode.codetest.domain.community.model.DiscussionVote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscussionVoteJpaJpaRepository extends JpaRepository<DiscussionVote, Long> {
+
+	Optional<DiscussionVote> findByVoterIdAndDiscussionId(Long voterId, Long discussionId);
+
+	boolean existsByVoterIdAndDiscussionId(Long voterId, Long discussionId);
+
+}
