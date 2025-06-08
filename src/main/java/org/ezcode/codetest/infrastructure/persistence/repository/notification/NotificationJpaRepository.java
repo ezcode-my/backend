c398902818de67ddb8353edd9d8933f26753bff6
SeungWoo Ryu <swstar21c@gmail.com>
2025-06-09 02:41:28 +0900

    feat : 도메인에서 notification 제거
    - 알림 기능은 자체적인 비즈니스 로직을 가지고 있지 않음
      - 알림 저장, 발송, 읽음 처리라는 간단한 기능만 필요
      - 때문에 이를 완전히 인프라 계층에서만 구현해도 문제가 없다고 판단했다.
    - 알림 데이터 저장은 nosql 사용하기로 함
      - 다양한 형태의 데이터 가질 수 있기 때문
      - 우선은 임시로 redis 사용
      - 추후 mongoDB 등으로 대체될 수 있음

--- src/main/java/org/ezcode/codetest/infrastructure/persistence/repository/notification/NotificationJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.ezcode.codetest.infrastructure.persistence.repository.notification;
-
-import org.ezcode.codetest.domain.notification.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationJpaRepository extends JpaRepository<Notification, Long> {
-}
