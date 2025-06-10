43f75bd79d5ec0347f19d603c4ffa9ba96f2848f
SeungWoo Ryu <swstar21c@gmail.com>
2025-06-10 17:42:59 +0900

    refactor : 오타 수정
    - ReplyVoteNotificationMapper에서 DiscussionVotePayload를 사용하고 있었음

--- src/main/java/org/ezcode/codetest/application/notification/event/mapper/ReplyVoteNotificationMapper.java
+++ src/main/java/org/ezcode/codetest/application/notification/event/mapper/ReplyVoteNotificationMapper.java
@@ -4,8 +4,8 @@ import java.time.LocalDateTime;
 
 import org.ezcode.codetest.application.notification.event.NotificationCreateEvent;
 import org.ezcode.codetest.application.notification.dto.event.ReplyVoteEvent;
-import org.ezcode.codetest.application.notification.event.payload.DiscussionVotePayload;
 import org.ezcode.codetest.application.notification.enums.NotificationType;
+import org.ezcode.codetest.application.notification.event.payload.ReplyVotePayload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,7 +23,7 @@ public class ReplyVoteNotificationMapper implements NotificationMapper<ReplyVote
 			event.principalName(),
 			NotificationType.COMMUNITY_REPLY_VOTED_UP,
 			"댓글에 추천을 받았습니다.",
-			new DiscussionVotePayload(event.replyId(), event.voterNickname()),
+			new ReplyVotePayload(event.replyId(), event.voterNickname()),
 			"/redirect",
 			false,
 			LocalDateTime.now()
