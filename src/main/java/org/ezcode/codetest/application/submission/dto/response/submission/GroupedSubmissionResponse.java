91ada3d5dd6052d583ed4a6bb332147fc8e5a842
thezz9 <thezz9@naver.com>
2025-06-13 18:37:20 +0900

    refactor : 그룹핑 처리 DTO로 책임 분리

--- src/main/java/org/ezcode/codetest/application/submission/dto/response/submission/GroupedSubmissionResponse.java
+++ src/main/java/org/ezcode/codetest/application/submission/dto/response/submission/GroupedSubmissionResponse.java
@@ -1,6 +1,8 @@
 package org.ezcode.codetest.application.submission.dto.response.submission;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.ezcode.codetest.domain.problem.model.entity.Problem;
 import org.ezcode.codetest.domain.submission.model.entity.Submission;
@@ -24,7 +26,24 @@ public class GroupedSubmissionResponse {
 	@Schema(description = "해당 문제에 대한 제출 목록")
 	private final List<SubmissionDetailResponse> submissions;
 
-	public GroupedSubmissionResponse(Problem problem, List<Submission> submissions) {
+	public static List<GroupedSubmissionResponse> groupByProblem(List<Submission> submissions) {
+		return submissions.stream()
+			.collect(Collectors.groupingBy(Submission::getProblem))
+			.entrySet()
+			.stream()
+			.map(entry -> createSorted(entry.getKey(), entry.getValue()))
+			.toList();
+	}
+
+	private static GroupedSubmissionResponse createSorted(Problem problem, List<Submission> submissions) {
+		List<Submission> sorted = submissions.stream()
+			.sorted(Comparator.comparing(Submission::getCreatedAt).reversed())
+			.toList();
+
+		return new GroupedSubmissionResponse(problem, sorted);
+	}
+
+	private GroupedSubmissionResponse(Problem problem, List<Submission> submissions) {
 		this.problemId = problem.getId();
 		this.problemDescription = problem.getDescription();
 		this.submissions = submissions.stream()
