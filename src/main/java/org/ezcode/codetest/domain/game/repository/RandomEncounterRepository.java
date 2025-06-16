f8979cf878475557355a663b7832fc4592f7cf67
chat26666 <chat26666@gmail.com>
2025-06-16 17:23:31 +0900

    feat: 랜덤 인카운터, 선택지 crud 추가

--- src/main/java/org/ezcode/codetest/domain/game/repository/RandomEncounterRepository.java
+++ src/main/java/org/ezcode/codetest/domain/game/repository/RandomEncounterRepository.java
@@ -1,4 +1,24 @@
 package org.ezcode.codetest.domain.game.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.ezcode.codetest.domain.game.model.entity.RandomEncounter;
+
 public interface RandomEncounterRepository {
+
+	Optional<RandomEncounter> findById(Long id);
+
+	boolean existsByName(String name);
+
+	Optional<RandomEncounter> findByName(String name);
+
+	RandomEncounter save(RandomEncounter encounter);
+
+	void delete(RandomEncounter encounter);
+
+	void deleteByName(String name);
+
+	List<RandomEncounter> findAll();
+
 }
--- src/main/java/org/ezcode/codetest/infrastructure/persistence/repository/game/EncounterChoiceRepositoryImpl.java
+++ src/main/java/org/ezcode/codetest/infrastructure/persistence/repository/game/EncounterChoiceRepositoryImpl.java
@@ -1,5 +1,10 @@
 package org.ezcode.codetest.infrastructure.persistence.repository.game;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.ezcode.codetest.domain.game.model.entity.EncounterChoice;
+import org.ezcode.codetest.domain.game.model.entity.RandomEncounter;
 import org.ezcode.codetest.domain.game.repository.EncounterChoiceRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +15,47 @@ import lombok.RequiredArgsConstructor;
 public class EncounterChoiceRepositoryImpl implements EncounterChoiceRepository {
 
 	private final EncounterChoiceJpaRepository encounterRepository;
+
+	@Override
+	public Optional<EncounterChoice> findById(Long id) {
+
+		return encounterRepository.findById(id);
+	}
+
+	@Override
+	public boolean existsByName(String name) {
+
+		return encounterRepository.existsByName(name);
+	}
+
+	@Override
+	public Optional<EncounterChoice> findByName(String name) {
+
+		return encounterRepository.findByName(name);
+	}
+
+	@Override
+	public EncounterChoice save(EncounterChoice encounter) {
+
+		return encounterRepository.save(encounter);
+	}
+
+	@Override
+	public void delete(EncounterChoice encounter) {
+
+		encounterRepository.delete(encounter);
+	}
+
+	@Override
+	public void deleteByName(String name) {
+
+		encounterRepository.deleteByName(name);
+	}
+
+	@Override
+	public List<EncounterChoice> findAll() {
+
+		return encounterRepository.findAll();
+	}
 
 }
