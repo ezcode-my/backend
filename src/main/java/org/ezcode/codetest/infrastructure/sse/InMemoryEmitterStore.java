efd53a6d29cd4a38aa9ea0ad80339cddc3be6f71
thezz9 <thezz9@naver.com>
2025-06-17 11:23:17 +0900

    hotfix : 제출 안 되는 부분 해결

--- src/main/java/org/ezcode/codetest/infrastructure/sse/InMemoryEmitterStore.java
+++ src/main/java/org/ezcode/codetest/infrastructure/sse/InMemoryEmitterStore.java
@@ -16,9 +16,6 @@ public class InMemoryEmitterStore implements EmitterStore {
 	@Override
 	public void save(String key, SseEmitter emitter) {
 		emitterMap.put(key, emitter);
-		emitter.onCompletion(() -> emitterMap.remove(key));
-		emitter.onTimeout(() -> emitterMap.remove(key));
-		emitter.onError((e) -> emitterMap.remove(key));
 	}
 
 	@Override
