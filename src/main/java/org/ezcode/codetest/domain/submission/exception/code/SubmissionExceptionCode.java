016ed02ca14e6a98884745f6a1dc4e4e2076582b
thezz9 <thezz9@naver.com>
2025-06-18 10:30:34 +0900

    feat : 채점하는 동안 중복 제출 방지 구현

--- src/main/java/org/ezcode/codetest/domain/submission/exception/code/SubmissionExceptionCode.java
+++ src/main/java/org/ezcode/codetest/domain/submission/exception/code/SubmissionExceptionCode.java
@@ -15,7 +15,8 @@ public enum SubmissionExceptionCode implements ResponseCode {
 	COMPILE_SERVER_ERROR(false, HttpStatus.BAD_GATEWAY, "컴파일 서버 오류"),
 	COMPILE_TIMEOUT(false, HttpStatus.GATEWAY_TIMEOUT, "컴파일 서버로부터 응답이 지연되고 있습니다."),
 	TESTCASE_TIMEOUT(false, HttpStatus.GATEWAY_TIMEOUT, "테스트케이스 채점 시간이 초과되었습니다."),
-	REDIS_SERVER_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR, "Redis 서버 연결 실패")
+	REDIS_SERVER_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR, "Redis 서버 연결 실패"),
+	ALREADY_JUDGING(false, HttpStatus.CONFLICT, "이미 해당 문제에 대한 채점이 진행 중입니다."),
 	;
 	private final boolean success;
 	private final HttpStatus status;
