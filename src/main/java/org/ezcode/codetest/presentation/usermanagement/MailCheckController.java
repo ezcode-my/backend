97f7cf7ccacd9be725a0486e00de9c6db1001ee4
minjee2758 <minjee2758@naver.com>
2025-06-19 17:14:48 +0900

    feature : 메일 전송 기능 추가

---
+++ src/main/java/org/ezcode/codetest/presentation/usermanagement/MailCheckController.java
@@ -0,0 +1,50 @@
+package org.ezcode.codetest.presentation.usermanagement;
+
+import java.util.HashMap;
+
+import org.ezcode.codetest.domain.user.model.entity.AuthUser;
+import org.ezcode.codetest.domain.user.service.MailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MailCheckController {
+	private final MailService mailService;
+	private int number; // 이메일 인증 숫자를 저장하는 변수
+
+	// 인증 이메일 전송
+	@PostMapping("/mailSend")
+	public HashMap<String, Object> mailSend(
+		@AuthenticationPrincipal AuthUser authUser) {
+		HashMap<String, Object> map = new HashMap<>();
+
+		try {
+			number = mailService.sendMail(authUser.getEmail());
+			String num = String.valueOf(number);
+
+			map.put("success", Boolean.TRUE);
+			map.put("number", num);
+		} catch (Exception e) {
+			map.put("success", Boolean.FALSE);
+			map.put("error", e.getMessage());
+		}
+
+		return map;
+	}
+
+	// 인증번호 일치여부 확인
+	@GetMapping("/mailCheck")
+	public ResponseEntity<?> mailCheck(@RequestParam String userNumber) {
+
+		boolean isMatch = userNumber.equals(String.valueOf(number));
+
+		return ResponseEntity.ok(isMatch);
+	}
+}
