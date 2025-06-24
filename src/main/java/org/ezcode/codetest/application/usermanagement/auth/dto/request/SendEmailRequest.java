a045faf24b4a6327cf51d5fc027fc60a554b5714
minjee2758 <minjee2758@naver.com>
2025-06-24 16:35:02 +0900

    Swagger 추가

--- src/main/java/org/ezcode/codetest/application/usermanagement/auth/dto/request/SendEmailRequest.java
+++ src/main/java/org/ezcode/codetest/application/usermanagement/auth/dto/request/SendEmailRequest.java
@@ -1,10 +1,13 @@
 package org.ezcode.codetest.application.usermanagement.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "이메일 전송 요청")
 public class SendEmailRequest {
+    @Schema(description = "인증 완료 후 리다이렉트할 URL")
     private String redirectUrl;
 }
