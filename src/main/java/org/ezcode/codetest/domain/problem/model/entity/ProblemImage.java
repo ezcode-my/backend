895e99f7c40fbdeef626a947729c934d4293de45
kimminu7 <rock9024@naver.com>
2025-06-24 16:25:00 +0900

    refactor : 엔티티 수정 ( 문제, 문제 이미지 ), 메소드 추가

--- src/main/java/org/ezcode/codetest/domain/problem/model/entity/ProblemImage.java
+++ src/main/java/org/ezcode/codetest/domain/problem/model/entity/ProblemImage.java
@@ -7,20 +7,31 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProblemImage {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	// 양방향 연관관계를 위한 setter
+	@Setter
 	@ManyToOne
 	@JoinColumn(name = "problem_id", nullable = false)
 	private Problem problem;
 
 	@Column(columnDefinition = "text", nullable = false)
 	private String imageUrl;
+
+	public ProblemImage(Problem problem, String imageUrl) {
+		this.problem = problem;
+		this.imageUrl = imageUrl;
+	}
 }
