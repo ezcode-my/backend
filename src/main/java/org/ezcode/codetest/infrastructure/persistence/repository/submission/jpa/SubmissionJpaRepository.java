43579b6b6550a3d47bb0e05cc06044c001c2d6e1
minjee2758 <minjee2758@naver.com>
2025-07-06 19:20:24 +0900

    총 문제 풀이 개수 추가

--- src/main/java/org/ezcode/codetest/infrastructure/persistence/repository/submission/jpa/SubmissionJpaRepository.java
+++ src/main/java/org/ezcode/codetest/infrastructure/persistence/repository/submission/jpa/SubmissionJpaRepository.java
@@ -1,10 +1,19 @@
 package org.ezcode.codetest.infrastructure.persistence.repository.submission.jpa;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.ezcode.codetest.domain.submission.model.entity.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SubmissionJpaRepository extends JpaRepository<Submission, Long> {
     List<Submission> findAllByUser_Id(Long userId);
+
+    @Query("""
+    SELECT COUNT(DISTINCT s.problem.id)
+    FROM Submission s
+    WHERE s.message = 'Accepted' AND s.user.id = :userId
+""")
+    Integer findAllByUserIdAndStatus(Long userId);
 }
