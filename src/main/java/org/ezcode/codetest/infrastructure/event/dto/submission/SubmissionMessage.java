0b25e7df0e92d21925ec2cdd39595df11384202c
thezz9 <thezz9@naver.com>
2025-07-08 09:41:33 +0900

    refactor: add submit prepare, ready

--- src/main/java/org/ezcode/codetest/infrastructure/event/dto/submission/SubmissionMessage.java
+++ src/main/java/org/ezcode/codetest/infrastructure/event/dto/submission/SubmissionMessage.java
@@ -1,5 +1,7 @@
 package org.ezcode.codetest.infrastructure.event.dto.submission;
 
+import org.ezcode.codetest.application.submission.dto.request.submission.CodeSubmitRequest;
+
 public record SubmissionMessage(
 
     String sessionKey,
@@ -13,4 +15,13 @@ public record SubmissionMessage(
     String sourceCode
 
 ) {
+    public static SubmissionMessage of(CodeSubmitRequest request, Long problemId, Long userId) {
+        return new SubmissionMessage(
+            request.sessionKey(),
+            problemId,
+            request.languageId(),
+            userId,
+            request.sourceCode()
+        );
+    }
 }
