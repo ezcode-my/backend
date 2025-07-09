395f2a08c0228bdddba4819efd08eb717771f14e
chat26666 <chat26666@gmail.com>
2025-07-09 11:22:42 +0900

    chore : change websocket config

--- src/main/java/org/ezcode/codetest/infrastructure/event/config/WebSocketConfig.java
+++ src/main/java/org/ezcode/codetest/infrastructure/event/config/WebSocketConfig.java
@@ -50,6 +50,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 			.setClientPasscode(mqPassword)
 			.setSystemLogin(mqUsername)
 			.setSystemPasscode(mqPassword)
+			.setSystemHeartbeatSendInterval(10000L)
+			.setSystemHeartbeatReceiveInterval(450000L)
 			.setUserDestinationBroadcast("/topic/simp-user-registry")
 			.setUserRegistryBroadcast("/topic/simp-user-registry");
 
