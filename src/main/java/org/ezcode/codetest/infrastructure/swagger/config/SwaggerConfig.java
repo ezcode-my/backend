3e9d02c07ee8a10b487cd36176c9a191e298972f
Taeik Kim <thezz9@naver.com>
2025-07-10 19:26:00 +0900

    fix: cors error in swagger (#167)

--- src/main/java/org/ezcode/codetest/infrastructure/swagger/config/SwaggerConfig.java
+++ src/main/java/org/ezcode/codetest/infrastructure/swagger/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @OpenAPIDefinition(
 	servers = {
 		@Server(
-			url = "http://localhost:8080",           // ← 슬래시 없이 호스트만!
+			url = "https://ezcode.my",           // ← 슬래시 없이 호스트만!
 			description = "Production server"
 		)
 	},
