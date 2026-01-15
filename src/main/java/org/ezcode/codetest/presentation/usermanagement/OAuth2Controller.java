e189c82662b75eff1ada23b532c254de38a75baf
Lim Minjee <limminjee@Limui-MacBookAir.local>
2026-01-15 13:45:49 +0900

    Revert "api redirect 주소 변경"
    This reverts commit a34c6452a95deb4fb6f4dd2a0d30162c161cc47e.

--- src/main/java/org/ezcode/codetest/presentation/usermanagement/OAuth2Controller.java
+++ src/main/java/org/ezcode/codetest/presentation/usermanagement/OAuth2Controller.java
@@ -47,7 +47,7 @@ public class OAuth2Controller {
 
         String target = UriComponentsBuilder.newInstance()
             .scheme("https")
-            .host("api.ezcode.my")
+            .host("ezcode.my")
             .path("/oauth2/authorization/" + provider)
             .build()
             .toUriString();
